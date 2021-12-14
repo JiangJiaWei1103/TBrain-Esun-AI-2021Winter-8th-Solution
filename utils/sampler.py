91d8577e1342258c40182bea71eefef61716f842
jiangjiawei1103 <waynechuang97@gmail.com>
2021-12-14 14:28:59 +0800

    feat: Class weights and production scheme
    Why:
    feat1 - Class weights
    Take effect of classes (i.e., shop_tags) into consideration.
    -----------------------------------------------------------------
    feat2 - Production scheme
    Align with the training scheme for production.
    
    What:
    feat1 - Class weights
    1. Compute class weight and fuse it with client-specific weight.
    -----------------------------------------------------------------
    feat2 - Production scheme
    1. Join weight matrix with `chid`s in y set.

--- utils/sampler.py
+++ utils/sampler.py
@@ -10,9 +10,11 @@ process.
 # Import packages
 import os 
 from tqdm import tqdm
+import math
 
 import pandas as pd 
 import numpy as np 
+from sklearn.utils.class_weight import compute_class_weight
 
 from metadata import *
 
@@ -23,21 +25,31 @@ class DataSampler:
     Parameters:
         t_end: int, the last time point taken into consideration when 
                generating X data
-        mode: str, scheme used to assign sample weight, default='naive'
-        imputation: str, way to impute missing values, default='median'
-            *Note: Inputation takes place when there's no historical 
-                   information can be used to create sample weights
-                   (e.g., shop_tag in y is the 'first' txn of client)
-        scale_method: str, scaling method used to eliminate dominance 
-                      effect of those always make transactions with 
-                      higher `txn_amt`, default='max'
+        horizon: int, predicting step
+        ds_cfg: dict, configuration for data sampler, commonly used 
+                parameters are introduced as follows:
+            mode-> str, scheme used to assign sample weight
+            imputation-> str, way to impute missing values
+                *Note: Impute when there's no historical information 
+                       can be used to create sample weights (e.g., 
+                       shop_tag in y is the 'first' txn of client)
+            scale_method-> str, method used to eliminate dominance 
+                           effect of those always make txns with 
+                           higher `txn_amt`
+            add_glob_cls_wt-> bool, whether to take class (shop_tag)
+                              weight into consideration
+        production: bool, whether to train model with all clients
+                    having txns in y
+            *Note: If True, then chids will align with y's chids
     '''
-    def __init__(self, t_end, mode='naive', imputation='median',
-                 scale_method='max'):
+    def __init__(self, t_end, horizon, ds_cfg, production=True):
         self._t_end = t_end
-        self._mode = mode
-        self._imputation = imputation
-        self._scale_method = scale_method
+        self._horizon = horizon
+        self._mode = ds_cfg['mode']
+        self._imputation = ds_cfg['imputation']
+        self._scale_method = ds_cfg['scale_method']
+        self._add_glob_cls_wt = ds_cfg['add_glob_cls_wt']
+        self._production = production
         self._setup()
         
     def run(self):
@@ -45,12 +57,15 @@ class DataSampler:
         '''
         if self._mode == 'naive':
             txn_amt_mean = (self._df
-                               .groupby(by=['chid', 'shop_tag'])['txn_amt']
-                               .mean())
+                                .groupby(by=['chid', 'shop_tag'])['txn_amt']
+                                .mean())
             txn_amt_mean = txn_amt_mean.reset_index(drop=False)
             weights = txn_amt_mean.pivot(index='chid', 
                                          columns='shop_tag',
                                          values='txn_amt')
+            if self._production:
+                # Align with chids in y
+                weights = weights.join(self._chids, how='right')
         else:
             pass
         
@@ -73,17 +88,41 @@ class DataSampler:
         shop_tags_.replace({shop_tag_idx: shop_tag for shop_tag_idx, shop_tag
                             in enumerate(LEG_SHOP_TAGS)}, inplace=True)
         
+        # Compute class weights
+        if self._add_glob_cls_wt:
+            # Perform worse so far 
+            global_weights = compute_class_weight('balanced', 
+                                                  sorted(shop_tags_.unique()),
+                                                  shop_tags_)
+#         global_weights = {shop_tag: weight for shop_tag, weight in 
+#                           zip(LEG_SHOP_TAGS, global_weights)}
+#         global_weights = (1e5 / shop_tags_.value_counts(sort=False)).to_dict()
+        
+        # Record client-specific weights
         for chid, shop_tag in tqdm(zip(chids, shop_tags_)):
             sample_weight.append(self._weights[chid][shop_tag])
         sample_weight = self._scale(chids, sample_weight)
-            
-        return sample_weight
+        
+        # Post process weights
+        sample_weight_ = []
+        for shop_tag, weight in zip(shop_tags_, sample_weight):
+#             sample_weight_.append(global_weights[shop_tag] * weight)
+#             sample_weight_.append(global_weights[shop_tag] * (weight+1))
+#             sample_weight_.append(weight * pow(1.005, global_weights[shop_tag]))
+            sample_weight_.append(math.exp(weight))#pow(, weight))
+    
+        return np.array(sample_weight_) 
     
     def _setup(self):
         '''Prepare data used to generate sample weights.
         '''
         self._df = pd.read_parquet("./data/raw/raw_data.parquet",
                                    columns=PK+['txn_cnt', 'txn_amt'])
+        if self._production:
+            pred_month = self._t_end + self._horizon
+            self._chids = (self._df[self._df['dt'] == pred_month]['chid']
+                               .unique())
+            self._chids = pd.DataFrame(index=self._chids)
         self._df = self._df[self._df['dt'] <= self._t_end]
         
     def _impute(self, weights):
